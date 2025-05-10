ddd9c06ef00edce4c2e46812517a173480d13884
kvng-dev <oghuanlan@gmail.com>
2025-05-10 12:26:00 +0100

    modified:   app/api/fetch-newest-note/route.ts

--- app/api/fetch-newest-note/route.ts
+++ app/api/fetch-newest-note/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(request: NextResponse) {
+export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const userId = searchParams.get("userId") || "";
 
